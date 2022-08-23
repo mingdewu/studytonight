58b698cc50ab01f61c363b1dcfc2f25f85d92894
mingdebaba <wmeadal@live.com>
2022-08-24 00:55:13 +0900

    ReadingCSV

---
+++ File_IO/ReadingCSV.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Arrays;
+
+public class ReadingCSV {
+	public static void main(String[] args) {
+		try {
+
+			List<List<String>> data = new ArrayList<>();// list of lists to store data
+			String file = "E:\\github_file\\studytonight\\File_IO\\demo.csv";// file path
+			FileReader fr = new FileReader(file);
+			BufferedReader br = new BufferedReader(fr);
+
+			// Reading until we run out of lines
+			String line = br.readLine();
+			while (line != null) {
+				List<String> lineData = Arrays.asList(line.split(","));// splitting lines
+				data.add(lineData);
+				line = br.readLine();
+			}
+
+			// printing the fetched data
+			for (List<String> list : data) {
+				for (String str : list)
+					System.out.print(str + " ");
+				System.out.println();
+			}
+			br.close();
+		} catch (Exception e) {
+			System.out.print(e);
+		}
+	}
+}
